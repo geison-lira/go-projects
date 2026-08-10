397d6324a4bf22bc272a099aef53564e31ca5d03
geison-lira <geisonvlf.dev@gmail.com>
2026-08-10 16:04:28 -0300

    feat: update main.go with the Golang Proverb

--- myhello/main.go
+++ myhello/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"example/myhello/greetings"
 	"fmt"
+
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hellow World")
 	fmt.Println(greetings.Welcome())
+	fmt.Println(quote.Go())
 }
