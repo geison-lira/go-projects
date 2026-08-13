f5522bdd861ae4e95d2a00dae1eda88946311c93
geison-lira <geisonvlf.dev@gmail.com>
2026-08-13 16:01:23 -0300

    style: remove blank lines

--- dialogues/oneway/client.go
+++ dialogues/oneway/client.go
@@ -16,15 +16,11 @@ func PrintError(err error) {
 func main() {
 	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:10001")
 	PrintError(err)
-
 	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
 	PrintError(err)
-
 	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
 	PrintError(err)
-
 	defer Conn.Close()
-
 	i := 0
 	for {
 		msg := strconv.Itoa(i)
--- dialogues/oneway/server.go
+++ dialogues/oneway/server.go
@@ -22,14 +22,10 @@ func PrintError(err error) {
 func main() {
 	ServerAddr, err := net.ResolveUDPAddr("udp", ":10001")
 	CheckError(err)
-
 	ServerConn, err := net.ListenUDP("udp", ServerAddr)
 	CheckError(err)
-
 	defer ServerConn.Close()
-
 	buf := make([]byte, 1024)
-
 	for {
 		n, addr, err := ServerConn.ReadFromUDP(buf)
 		fmt.Println("Received", string(buf[0:n]), "from", addr)
